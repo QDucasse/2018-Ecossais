a8427bb16d8beca480976daa55f1ab0b2f0be812
Quentin <quentin@lses-MacBook-Pro.local>
2018-02-26 20:16:18 +0100

    Premiers tests

---
+++ src/testEcoss.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 19:56:49 2018
+
+@author: quentin
+"""
+
+import unittest
+
+
+class TestJoueur(unittest.TestCase):
+    def testInitialisation:
+    def testPlacer:
+    def testPiocher:
+        
+class TestCarte(unittest.TestCase):
+    def testInitialisation
+    
+class TestPlateau(unittest.TestCase):
+    def testInitialisation
+    
+class TestBorne(unittest.TestCase):
+    
+class TestGroupeCartes(unittest.TestCase):
+    
+class TestJeu(unittest.TestCase):
+    
+
+
