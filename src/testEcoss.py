b8937a76833c2cfb61720d8219a8d5f3d5857b9b
Matthix7 <37193404+Matthix7@users.noreply.github.com>
2018-03-11 16:16:45 +0100

    Update testEcoss.py

--- src/testEcoss.py
+++ src/testEcoss.py
@@ -26,13 +26,14 @@ class TestJoueur(unittest.TestCase):
     # plateau = []
     # for i in range(7):
     #     plateau.append(['  ']*9)
-    # plateau[3]=['XX','XX','XX','XX','XX','XX','XX','XX','XX']
+    # # plateau[3]=['XX','XX','XX','XX','XX','XX','XX','XX','XX']
     
     plateau = Plateau(9)
     
     partie = Jeu(plateau)
     
-    Toto = Joueur(6, 1, plateau, partie)
+    Toto = Joueur(6, randint(1,3), plateau, partie)
+    
     
     # def testInstance:    inutile, aucun échec possible
     #     Toto = Joueur(5, 1, plateau, partie)
@@ -54,21 +55,28 @@ class TestJoueur(unittest.TestCase):
         
         carte = Toto[no]   
         
-        with self.assertRaises(Exception) as err1:
-            Toto.placer(no, (ord, abs))
-        
-        with self.assertRaises(Exception) as err2:
-            Toto.placer(randint(1,6), (abs, ord))
-        self.assertTrue('Emplacement occupé' in err2.exception)
-        
         
         self.assertEqual(Toto.plateau[ord][abs], '{0}{1}'.format(carte.couleur, carte.valeur))
         
         
+    
+    def testPeutJouer(self):
+        test1 = {1:(0,0) ,1:(1,8) ,1:(2,8) ,1:(2,5) ,1:(0,8) ,2:(8,8) ,2:(7,0) ,2:(6,0) ,2:(8,0) ,2:(6,8)}
+        test2 = {2:(0,0) ,2:(1,8) ,2:(2,8) ,2:(2,5) ,2:(0,8) ,1:(8,8) ,1:(7,0) ,1:(6,0) ,1:(8,0) ,1:(6,8), 2:(3,0), 2:(3,8), 1:(3,0), 1:(3,8), 2:(9,3), 2:(4,9), 1:(2,9), 1:(-1,3), 1:(0,1), 1:(2,3), 2:(7,1), 2:(6,3)}
+        
+        plateau = Plateau(9)
+        plateau.tapis[0][1] = 'A6'
+        plateau.tapis[2][3] = 'A6'
+        plateau.tapis[7][1] = 'A6'
+        plateau.tapis[6][3] = 'A6'
         
         
+        for Jt,pos in self.test1.items():
+            self.assertEqual(Joueur(5, Jt, plateau, Jeu(plateau)).peutJouer(pos), True)
+            
+        for Jt,pos in self.test2.items():
+            self.assertEqual(Joueur(5, Jt, plateau, Jeu(plateau)).peutJouer(pos), False)
         
-     def testPeutJouer(self):
          
 #     def testPiocher(self):
 #         
@@ -98,3 +106,4 @@ class TestJoueur(unittest.TestCase):
 
 # if  __name__  == "__main__":
 #     unittest.main() 
+
