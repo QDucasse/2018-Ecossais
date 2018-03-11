878bd62b5ff76dd4101e933fde5287e5fb09356c
Quentin <quentin@lses-MacBook-Pro.local>
2018-03-11 11:12:31 +0100

    Placer plus cohérent mais à vérifier

--- src/joueur.py
+++ src/joueur.py
@@ -68,31 +68,34 @@ class Joueur(list):
         Carte choisie
         Position visée sous la forme d'un tuple
         '''
-
-        self.plateau.tapis[position[0]][position[1]]=str(self[no_carte]) 
+        
         #Placement de la carte sur le tapis
+        self.plateau.tapis[position[0]][position[1]]=str(self[no_carte]) 
         
-        self[no_carte].position=str(position)                           
         #Rafraîchissement de la position de la carte en elle-même
+        self[no_carte].position=str(position)                           
         
-        self.jeu.rafraichissementIntegral
         #Rafraîchissement des bornes pour y faire apparaître la carte
-        
-        borneEnCours=self.jeu.ensembleBorne[position[1]]
+        self.jeu.rafraichissementIntegral
+       
         #On mémorise la borne sur laquelle la carte a été placée
-        if self.jeu.joueurCourant==1:
-            if borneEnCours.g1.force!=0 and borneEnCours.premierComplete==0:
-                borneEnCours.premierComplete=1
-                borneEnCours.comparer()
-        #On vérifie que le groupe de carte du côté du joueur venant de jouer est incomplet et s'il vient
-        #d'être complété, on change la valeur de PremierComplété
-        else:
-            if borneEnCours.g1.force==0 and borneEnCours.premierComplete==0:
-                borneEnCours.premierComplete=2
-                borneEnCours.comparer()
+        borneEnCours=self.jeu.ensembleBorne[position[1]]
         
-        del(self[no_carte])
+        #Si jamais un des groupes est complété, on change la valeur de premierComplete !
+        if self.jeu.joueurCourant==1:
+            if borneEnCours.g1.estComplet() and not borneEnCours.g2.estComplet():
+                self.jeu.borneEnCours.premierComplete=1        
+        else:
+            if borneEnCours.g1.estComplet() and not borneEnCours.g2.estComplet():
+                self.jeu.borneEnCours.premierComplete=1
+        
+        #Comparaison si jamais les deux groupes sont complets
+        if borneEnCours.g1.estComplet() and borneEnCours.g2.estComplet():
+            borneEnCours.comparer()
+        
         #Suppression de la carte de la main du joueur
+        del(self[no_carte])
+        
    
     def peutJouer(self,position):
         '''
