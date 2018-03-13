ae8ce8904c0424e8eed04af924cf857e5b3758aa
Quentin Ducasse <quentin.ducasse@free.fr>
2018-03-13 21:26:00 +0100

    ALL GREEN !!

--- src/testBorne.py
+++ src/testBorne.py
@@ -21,5 +21,89 @@ from numpy.random import randint
 
 
 class TestBorne(unittest.TestCase):
+    
+    
 
-    def test
+    def testInit(self):
+        p=Plateau(9)
+        p.tapis[0][0]=Carte(1,'A')
+        p.tapis[1][0]=Carte(2,'B')
+        p.tapis[2][0]=Carte(3,'C')
+        p.tapis[4][0]=Carte(4,'D')
+        p.tapis[5][0]=Carte(5,'E')
+        p.tapis[6][0]=Carte(6,'F')
+        b=Borne(0,p)
+        self.assertEquals(str(b.g1),str(GroupeCartes(Carte(1,'A'),Carte(2,'B'),Carte(3,'C'))))
+        self.assertEquals(str(b.g2),str(GroupeCartes(Carte(4,'D'),Carte(5,'E'),Carte(6,'F'))))
+        
+    def testPeutComparer(self):
+        p=Plateau(9)
+        p.tapis[0][0]=Carte(1,'A')
+        p.tapis[1][0]=Carte(2,'B')
+        p.tapis[2][0]=Carte(3,'C')
+        p.tapis[4][0]=Carte(4,'D')
+        p.tapis[5][0]=Carte(5,'E')
+        p.tapis[6][0]=Carte(6,'F')
+        b=Borne(0,p)
+        
+        #Vérification peutComparer dans un cas Vrai
+        self.assertEquals(b.peutComparer(),True)
+        
+        #Vérification peutComparer dans un cas Faux
+        p.tapis[0][0]=Carte(0,'X')
+        b=Borne(0,p)
+        self.assertEquals(b.peutComparer(),False)
+    
+    def testComparerCondition1(self):
+        p=Plateau(9)
+        p.tapis[0][0]=Carte(1,'A')
+        p.tapis[1][0]=Carte(2,'A')
+        p.tapis[2][0]=Carte(3,'A')
+        p.tapis[4][0]=Carte(4,'D')
+        p.tapis[5][0]=Carte(5,'E')
+        p.tapis[6][0]=Carte(6,'F')
+        b=Borne(0,p)
+        b.g1.calculForce()
+        b.g2.calculForce()
+        b.comparer()
+        
+        #Cas dans lequel J1 gagne, son groupe étant de force 5 contre g2 de force 2
+        self.assertEquals(p.tapis[3][0],'J1')
+      
+    def testComparerCondition2(self):
+        #Cas dans lequel les deux groupes sont de force identique => on prnd en compte le total de points
+        p=Plateau(9)
+        p.tapis[0][0]=Carte(1,'A')
+        p.tapis[1][0]=Carte(2,'A')
+        p.tapis[2][0]=Carte(3,'A')
+        p.tapis[4][0]=Carte(4,'B')
+        p.tapis[5][0]=Carte(5,'B')
+        p.tapis[6][0]=Carte(6,'B')
+        b=Borne(0,p)
+        b.g1.calculForce()
+        b.g2.calculForce()
+        b.comparer()
+        
+        self.assertEquals(p.tapis[3][0],'J2')
+        
+    def testComparerCondition3(self):
+        #Cas dans lequel les deux groupes sont de force identique => on prnd en compte le total de points
+        p=Plateau(9)
+        p.tapis[0][0]=Carte(1,'C')
+        p.tapis[1][0]=Carte(3,'D')
+        p.tapis[2][0]=Carte(3,'B')
+        p.tapis[4][0]=Carte(1,'B')
+        p.tapis[5][0]=Carte(3,'E')
+        p.tapis[6][0]=Carte(3,'F')
+        b=Borne(0,p)
+        b.g1.calculForce()
+        b.g2.calculForce()
+        b.premierComplete=1
+        b.comparer()
+        
+        self.assertEquals(p.tapis[3][0],'J1')
+        
+        
+        
+if  __name__  == "__main__":
+    unittest.main() 
