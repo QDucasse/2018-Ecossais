0b9ccf60b6986b86a06322f20f15c43cbe7024fa
Quentin Ducasse <quentin.ducasse@free.fr>
2018-03-14 21:10:45 +0100

    ALL GREEN BB !

--- src/testBorne.py
+++ src/testBorne.py
@@ -33,8 +33,8 @@ class TestBorne(unittest.TestCase):
         p.tapis[5][0]=Carte(5,'E')
         p.tapis[6][0]=Carte(6,'F')
         b=Borne(0,p)
-        self.assertEquals(str(b.g1),str(GroupeCartes(Carte(1,'A'),Carte(2,'B'),Carte(3,'C'))))
-        self.assertEquals(str(b.g2),str(GroupeCartes(Carte(4,'D'),Carte(5,'E'),Carte(6,'F'))))
+        self.assertEqual(str(b.g1),str(GroupeCartes(Carte(1,'A'),Carte(2,'B'),Carte(3,'C'))))
+        self.assertEqual(str(b.g2),str(GroupeCartes(Carte(4,'D'),Carte(5,'E'),Carte(6,'F'))))
         
     def testPeutComparer(self):
         p=Plateau(9)
@@ -47,12 +47,12 @@ class TestBorne(unittest.TestCase):
         b=Borne(0,p)
         
         #Vérification peutComparer dans un cas Vrai
-        self.assertEquals(b.peutComparer(),True)
+        self.assertEqual(b.peutComparer(),True)
         
         #Vérification peutComparer dans un cas Faux
         p.tapis[0][0]=Carte(0,'X')
         b=Borne(0,p)
-        self.assertEquals(b.peutComparer(),False)
+        self.assertEqual(b.peutComparer(),False)
     
     def testComparerCondition1(self):
         p=Plateau(9)
@@ -68,7 +68,7 @@ class TestBorne(unittest.TestCase):
         b.comparer()
         
         #Cas dans lequel J1 gagne, son groupe étant de force 5 contre g2 de force 2
-        self.assertEquals(p.tapis[3][0],'J1')
+        self.assertEqual(p.tapis[3][0],'J1')
       
     def testComparerCondition2(self):
         #Cas dans lequel les deux groupes sont de force identique => on prnd en compte le total de points
@@ -84,7 +84,7 @@ class TestBorne(unittest.TestCase):
         b.g2.calculForce()
         b.comparer()
         
-        self.assertEquals(p.tapis[3][0],'J2')
+        self.assertEqual(p.tapis[3][0],'J2')
         
     def testComparerCondition3(self):
         #Cas dans lequel les deux groupes sont de force identique => on prnd en compte le total de points
@@ -101,9 +101,19 @@ class TestBorne(unittest.TestCase):
         b.premierComplete=1
         b.comparer()
         
-        self.assertEquals(p.tapis[3][0],'J1')
+        self.assertEqual(p.tapis[3][0],'J1')
         
-        
+    def testPremierComplete(self,j=Jeu()):
+        p=j.plateau
+        p.tapis[0][0]=Carte(1,'C')
+        p.tapis[1][0]=Carte(3,'D')
+        p.tapis[2][0]=Carte(3,'B')
+        p.tapis[4][0]=Carte(1,'B')
+        p.tapis[5][0]=Carte(3,'E')
+        p.tapis[6][0]=Carte(0,'X')
+        b=Borne(0,p)
+        b.verifPremierComplete(j)
+        self.assertEqual(b.premierComplete,1)
         
 if  __name__  == "__main__":
     unittest.main() 
